98faef84f010400f175aef1610909812f4b5d167
yuto14 <programming1410@gmail.com>
2020-06-08 15:54:26 +0900

    [finish]応用課題6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,15 +1,19 @@
 class BookCommentsController < ApplicationController
 	def create
 		@book = Book.find(params[:book_id])
+		#comment = BookComment.new(book_comment_params)
+		#comment.user_id = current_user.idの２行を１行にまとめた記述
 		comment = current_user.book_comments.new(book_comment_params)
 		comment.book_id = @book.id
 		comment.save
-		
+		@book_comment = BookComment.new
 	end
 
 	def destroy
-		BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-		
+		@book = Book.find(params[:book_id])
+		comment = BookComment.find(params[:id])
+		comment.destroy
+		@book_comment = BookComment.new
 	end
 
     private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,11 @@ class FavoritesController < ApplicationController
 		@book = Book.find(params[:book_id])
 		favorite = current_user.favorites.new(book_id: @book.id)
 		favorite.save
-		
 	end
 
 	def destroy
 		@book = Book.find(params[:book_id])
 		favorite = current_user.favorites.find_by(book_id: @book.id)
 		favorite.destroy
-		
 	end
 end
